b8a903db7ed54be7a097a72a6215180201e6d2d4
Brian  Doyle <briandoyle81@gmail.com>
2022-12-01 11:30:35 -0500

    Day 1 part 2

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ I've decided to use this one to learn Golang
 # 01
 
 The problem so far is trivial, most of my time here is spent learning how to get the data into the program, set up a map of ints to int array/slices, and get the data in that.
+
+For part 2, the natural solution is a heap.  I found a min-heap and couldn't figure out how to modify it, so I just stored the negative of each value.
+
+I also learned that `go run main.go` won't load other files, even if they're in the same package.
--- day-01/main.go
+++ day-01/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
 	"os"
 	"strconv"
@@ -53,21 +54,46 @@ func sumArray(array []int) int {
 	return result
 }
 
+// Part 1
+// func main() {
+//     data := importData()
+
+// 	largestValue := 0
+// 	var largestKey int
+
+// 	totals := make(map[int]int)
+
+// 	for key, val := range data {
+// 		totals[key] = sumArray(val)
+// 		if totals[key] > largestValue {
+// 			largestValue = totals[key]
+// 			largestKey = key
+// 		}
+// 	}
+
+// 	fmt.Println(largestKey, largestValue)
+// }
+
+// Part 2
+
 func main() {
     data := importData()
 
-	largestValue := 0
-	var largestKey int
+	// Store values in a min heap
+	// TODO: Figure out how to make this a max heap instead
+	h := &IntHeap{0, 0, 0}
+	heap.Init(h)
 
-	totals := make(map[int]int)
-
-	for key, val := range data {
-		totals[key] = sumArray(val)
-		if totals[key] > largestValue {
-			largestValue = totals[key]
-			largestKey = key
-		}
+	for _, val := range data {
+		heap.Push(h, sumArray(val)*-1) // Hack to work with min heap
 	}
 
-	fmt.Println(largestKey, largestValue)
+	topThreeTotal := 0
+
+	// There must be something more elegant than a for loop here
+	for i := 0; i < 3; i++ {
+		topThreeTotal += heap.Pop(h).(int) // TODO: What is this .(int) and why does it work?
+	}
+
+	fmt.Println(topThreeTotal * -1)
 }
